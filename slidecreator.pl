131d34679528f57887a2b085d9d4c2653baf5563
Patrick McSweeney <mcsweeney.patrick@gmail.com>
2012-04-10 22:50:40 +0100

    First attempt at the pecha kucha creator

---
+++ slidecreator.pl
@@ -0,0 +1,105 @@
+#!/usr/bin/perl
+
+use strict;
+use warnings;
+use POSIX qw/ceil/;
+
+my $number_of_slides = 20;
+my $fabula_size = 500;
+my $theme = $ARGV[1] || "winter";
+
+open FILE, $ARGV[0];
+
+my @words = ();
+foreach my $line (<FILE>)
+{
+	$line =~ s/[^a-zA-Z0-9]/ /g;
+	$line = lc $line;
+	
+	push @words, split(/ +/, $line);
+
+}
+close FILE;
+
+my %word_set = ();
+foreach my $word (@words)
+{
+	$word_set{$word}++;
+}
+
+open NGRAMS, "2008grams.tsv";
+my $ngram_count = 0;
+my %ngram_set = ();
+foreach my $line (<NGRAMS>)
+{
+	#word	year	occurences	pages	texts
+	my @bits = split /\t/, $line;
+	$ngram_count += $bits[2];
+	if($word_set{$bits[0]})
+	{
+		$ngram_set{$bits[0]} = $bits[2];
+	}
+}
+close NGRAMS;
+
+use Data::Dumper;
+
+#print Dumper(%ngram_set);
+
+print STDERR "there are ".scalar @words." words\n";
+
+my $word_count = scalar @words;
+my $section_size = ceil($word_count/$number_of_slides);
+
+my @slide_words = ();
+my $slide_count = 0;
+while(scalar @words > 0){
+	$slide_words[$slide_count] = "";
+	my $best_score = 0;
+	foreach my $word (splice(@words, 0, $section_size))
+	{
+		#print "the word $word is ".$word_set{$word}/$word_count." of the document\n";
+		if(!$ngram_set{$word})
+		{
+			print STDERR "Word: $word is not in the ngram set. It is ignored\n";
+			next;
+		}
+
+		my $word_score = ($word_set{$word} * $ngram_count) / ($ngram_set{$word} * $word_count);
+		if($word_score > $best_score)
+		{
+			$best_score = $word_score;
+			$slide_words[$slide_count] = $word;
+		}
+	}
+	print STDERR "\n\n END OF SLIDE $slide_count. Word: ".$slide_words[$slide_count].". Score: $best_score\n\n";
+	$slide_count++;
+}
+
+my %images = ();
+my @slides = ();
+foreach my $keyword (@slide_words)
+{
+	if(! -e $keyword.$fabula_size.".fab")
+	{
+		`java -jar flickrfab.jar $keyword $fabula_size -noise 4`;
+	}
+	
+	if(! defined $images{$keyword} )
+	{
+		my $output = `java -jar tmb.jar $keyword$fabula_size.fab $number_of_slides $theme -brief -o -`;
+	
+		my @output_lines = split /\n/, $output;
+		
+		$images{$keyword} = \@output_lines;
+	}
+
+	
+	push @slides, shift(@{$images{$keyword}});
+}
+
+foreach my $slide (@slides)
+{
+	my $keyword = shift @slide_words;
+	print "<img src='$slide' alt='$keyword' title='$keyword' />\n";
+}
